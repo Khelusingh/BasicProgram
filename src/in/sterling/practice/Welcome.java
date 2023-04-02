f691abab369379efdee6e9cdeb8c193aa845aec5
Khelusingh <khelendrapsingh@gmail.com>
2023-04-02 11:19:00 +0530

    Update 3

--- src/in/sterling/practice/Welcome.java
+++ src/in/sterling/practice/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 		for (int i = 1; i <= 100; i++) {
 			System.out.println("The numbers are " + i + " ");
 		}
+		System.out.println("Java is programming language and is easy to learn");
 	}
 
 }

46a223f9512a5ace768e176dd137360889ae5e58
Khelusingh <khelendrapsingh@gmail.com>
2023-04-02 11:09:23 +0530

    update2

--- src/in/sterling/practice/Welcome.java
+++ src/in/sterling/practice/Welcome.java
@@ -2,8 +2,9 @@ package in.sterling.practice;
 
 public class Welcome {
 	public static void main(String[] args) {
-		System.out.println("Welcome");
-		System.out.println("");
+		for (int i = 1; i <= 100; i++) {
+			System.out.println("The numbers are " + i + " ");
+		}
 	}
 
 }
